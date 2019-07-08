22e053886e8be4c4a37c5684aa6672439d03c09c
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-07-08 15:04:56 +0100

    fix flask version to work with werkzeug = <0.15

--- conftest.py
+++ conftest.py
@@ -7,15 +7,14 @@ from app import create_app
 @pytest.fixture
 def client():
     app = create_app('UnitTestConfig')
-
     client = app.test_client()
-
     old_open = client.open
 
     def open_with_auth(*args, **kwargs):
         kwargs['headers'] = kwargs.get('headers') or {}
         kwargs['headers']['Authorization'] = _basic_auth_str('admin', 'secret')
         return old_open(*args, **kwargs)
+
     client.open = open_with_auth
 
     yield client

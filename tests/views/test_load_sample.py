7f194823279b988bb05986205e801caf72452fa2
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2019-07-17 15:06:37 +0100

    916 source of truth (#15)
    * fix flask version to work with werkzeug = <0.15
    
    * tests pass, needs to be tested manually
    
    * fixed some more tests
    
    * pipfile altered
    
    * clean up
    
    * removed rabboit context tests
    
    * altered load_sample to use app.config['blah']
    
    * update to use tagged release of sample-loader

--- tests/views/test_load_sample.py
+++ tests/views/test_load_sample.py
@@ -1,6 +1,8 @@
 from io import BytesIO
 from unittest.mock import ANY, patch
 
+from config import UnitTestConfig
+
 
 def test_get_upload_sample_page(client, requests_mock):
     requests_mock.get('/actionPlans/4c27fcbf-e069-44db-a239-99c9cd70a1d0', json={
@@ -30,6 +32,13 @@ def test_upload_social_sample_file(client):
                                data={'sample': (BytesIO(b'header\nline'), 'sample.csv')})
 
     load_sample_patch.assert_called_once_with(ANY, collection_exercise_id='4c27fcbf-e069-44db-a239-99c9cd70a1d0',
-                                              action_plan_id='4c27fcbf-e069-44db-a239-99c9cd70a1d0')
+                                              action_plan_id='4c27fcbf-e069-44db-a239-99c9cd70a1d0',
+                                              host=UnitTestConfig.RABBITMQ_HOST,
+                                              port=UnitTestConfig.RABBITMQ_PORT,
+                                              vhost=UnitTestConfig.RABBITMQ_VHOST,
+                                              exchange=UnitTestConfig.RABBITMQ_EXCHANGE,
+                                              user=UnitTestConfig.RABBITMQ_USER,
+                                              password=UnitTestConfig.RABBITMQ_PASSWORD,
+                                              queue_name=UnitTestConfig.RABBITMQ_QUEUE)
 
     assert response.status_code == 302
